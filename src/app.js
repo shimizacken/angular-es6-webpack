b2ffca625148fbf66e85a4d3ed9e38e0e52e2b17
Shimi Zacken <shimizacken@gmail.com>
2017-03-18 15:26:17 +0200

    components added

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import angular from 'angular';
+import Components from './components/components'
+
+let app = angular.module('app', [Components]);
---
+++ src/components/components.js
@@ -0,0 +1,6 @@
+import angular from 'angular';
+
+import Header from './header/header-component';
+
+export default angular.module('Components', [])
+    .component('Header', Header);
